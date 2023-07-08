64b06672f49233abe0461ee25b3d3e1e1ebc90c9
pedo4 <89007213+pedo4@users.noreply.github.com>
2023-07-08 12:07:05 +0200

    Add files via upload

--- Templates/app.py
+++ Templates/app.py
@@ -1,26 +1,32 @@
-from flask import Flask, jsonify, render_template,session,redirect
+from flask import Flask, jsonify, render_template, session, redirect
 from waitress import serve
-from code.auth_utils import is_logged_in
-from code.templ_utils import get_templates
-
+from util.auth_utils import is_logged_in
+from util.templ_utils import get_templates
 
 app = Flask(__name__)
+app.secret_key = "mysecret"
 
 # Rotta per ottenere i template
 @app.route('/template', methods=['GET'])
-def get_templates():
-    if(is_logged_in(session)):
-        return jsonify(get_templates)
+def get_temp():
+    if is_logged_in(session):
+        templates = get_templates()
+        return jsonify(templates)
     else:
         return redirect("/login")
 
 # Pagina principale
 @app.route('/templ', methods=['GET'])
 def index():
-    if(is_logged_in(session)):
+    print(session)
+    if is_logged_in(session):
         return render_template('template.html')
     else:
         return redirect("/login")
 
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=80,threads=4)
+   serve(app, host='0.0.0.0', port=80, threads=4)
+
+
+#if __name__ == '__main__':
+#   app.run(host='0.0.0.0', port=80)
---
+++ Templates/util/templ_utils.py
@@ -0,0 +1,16 @@
+def get_templates():
+    templates = [
+    {
+        "type": "ecommerce",
+        "icon": "e-commerce.png"
+    },
+    {
+        "type": "news",
+        "icon": "news.png"
+    },
+    {
+        "type": "company",
+        "icon": "enterprise.png"
+    }
+    ]
+    return templates
