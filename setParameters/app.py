fa46fa591580277d5ca7c8452c145396e46e58f3
pedo4 <89007213+pedo4@users.noreply.github.com>
2023-07-08 12:07:31 +0200

    Add files via upload

--- setParameters/app.py
+++ setParameters/app.py
@@ -1,16 +1,28 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template,redirect,jsonify
 from flask_cors import CORS
-from code.parameters import get_parameter_names
+from waitress import serve
 
 app = Flask(__name__)
 CORS(app)
 
+
+ 
+    
+
 @app.route('/par', methods=['POST'])
 def handle_par_request():
     template_type = request.form['templateType']
-    parameter_names = get_parameter_names(template_type)
+    if template_type == 'ecommerce':
+        return redirect('/ecomm')
+    elif template_type == 'news':
+        return redirect('/news')
+    else:
+        return redirect('/corp')
     
-    return render_template('template_params.html', parameter_names=parameter_names)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+   serve(app, host='0.0.0.0', port=80, threads=4)
+
+
+#if __name__ == '__main__':
+#   app.run(host='0.0.0.0', port=80)
